c0f6af5cc0396bd07bf5c2f61ff5d10417dd3485
Fabio Goncalves <fabiogsantos@gmail.com>
2019-05-02 14:21:09 -0300

    Sessao 8 aula 41 a 43 - Implemento cadastro Cargo. Botao Excluir do Departamento continua sem funcionar

--- src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
@@ -1,15 +1,21 @@
 package com.mballem.curso.boot.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.domain.Departamento;
 import com.mballem.curso.boot.service.CargoService;
+import com.mballem.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
@@ -17,6 +23,9 @@ public class CargoController {
 
 	@Autowired
 	private CargoService service;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
 	
 	  @GetMapping("/cadastrar")
 	  public String cadastrar(Cargo cargo) {
@@ -29,9 +38,15 @@ public class CargoController {
 	  }
 
 	  @PostMapping("/salvar")
-	  public String salvar(Cargo cargo) {
+	  public String salvar(Cargo cargo, RedirectAttributes attr) {
 		  service.salvar(cargo);
+		  attr.addFlashAttribute("success","Cargo inserido com sucesso.");
 		  return "redirect:/cargos/cadastrar";
+	  }
+	  
+	  @ModelAttribute("departamentos")
+	  public List<Departamento> listaDeDepartamentos() {
+		  return departamentoService.buscarTodos(); 
 	  }
 
 		 
